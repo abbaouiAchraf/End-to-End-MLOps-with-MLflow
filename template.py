5b5aabc067c4f020ed4990ac3d2f947bb8ad3c74
ABBAOUI-Achraf <achraf.abbaoui@etu.uae.ac.ma>
2023-09-18 00:57:58 +0100

    Generating files/folders from template.py

---
+++ template.py
@@ -0,0 +1,49 @@
+# it will generate folders structure and files for a new project when ever you run it
+import os
+from pathlib import Path
+import logging
+
+# create logger to log the info in a certain format defined below
+logging.basicConfig(level=logging.INFO, format=' [%(asctime)s]: %(message)s:')
+
+project_name = 'MLOpsProject'
+
+# create a list of folders to be created
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath) # convert to path object, parsing to windows or linux path format
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir}, for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+    else:
+        logging.info(f"File already exists: {filename}")
