b3e9368cb4033ac4372c3ff73bbbb95888bbfaf2
ABBAOUI-Achraf <achraf.abbaoui@etu.uae.ac.ma>
2023-10-01 20:18:39 +0100

    DecisionTreeClassifier Update

--- src/MLOpsProject/components/model_evaluation.py
+++ src/MLOpsProject/components/model_evaluation.py
@@ -49,6 +49,6 @@ class ModelEvaluation:
 
             # Model registry does not work with file store
             if tracking_url_type_store != "file":
-                mlflow.sklearn.log_model(model, "model", registered_model_name="RandomTreeClassifier")
+                mlflow.sklearn.log_model(model, "model", registered_model_name="DecisionTreeClassifier")
             else:
                 mlflow.sklearn.log_model(model, "model")
